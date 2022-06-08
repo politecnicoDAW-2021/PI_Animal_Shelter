ae4d5842883259bae16f33ed2396d075b81c6364
laranatoli <franciscolaranatoli@gmail.com>
2022-06-08 13:51:51 +0200

    build(back): interfaces

---
+++ back/src/common/interfaces/social_media.interface.ts
@@ -0,0 +1,6 @@
+export interface SocialMedia {
+  id: number;
+  type: string;
+  url: string;
+  icon: string;
+}
--- back/src/common/interfaces/user.interface.ts
+++ back/src/common/interfaces/user.interface.ts
@@ -1,9 +1,9 @@
 export interface User{
     id:number
-    username:string
-    email:string
+    username: string
+    email: string
     rol: string
-    city:string
-    id_shelter:Shelter
-    picture:string
+    city: string
+    id_shelter: Shelter
+    picture: string
 }
