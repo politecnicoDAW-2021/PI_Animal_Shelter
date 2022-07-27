477c000a72768fbd9d81df567b092f3ae5198fb9
laranatoli <franciscolaranatoli@gmail.com>
2022-07-27 22:07:24 +0200

    build: typeORM config

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,32 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { typeOrmConfig } from './config/typeOrm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { AnimalEntity } from './database/entities/animal/animal.entity';
+import { SpecieEntity } from './database/entities/animal/specie.entity';
+import { ShelterEntity } from './database/entities/shelter/shelter.entity';
+import { CreditCardEntity } from './database/entities/user/credit_card.entity';
+import { PasswordEntity } from './database/entities/user/password.entity';
+import { WishlistEntity } from './database/entities/common/wishlist.entity';
+import { UserEntity } from './database/entities/user/user.entity';
+import { SocialMediaEntity } from './database/entities/shelter/social_media.entity';
+import { MediaEntity } from './database/entities/animal/media.entity';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forFeature([
+      CreditCardEntity,
+      PasswordEntity,
+      WishlistEntity,
+      UserEntity,
+      SocialMediaEntity,
+      ShelterEntity,
+      AnimalEntity,
+      SpecieEntity,
+      MediaEntity,
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
