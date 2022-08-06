74481144fb5d768b7ba25225a8e6b8cc44947b2f
davidmr163 <davidmonteroruiz163@gmail.com>
2022-08-06 13:21:23 +0200

    build(front): fxlayout angular added

--- front/src/app/shared/material/material.module.ts
+++ front/src/app/shared/material/material.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+//import { NgxMatFileInputModule } from '@angular-material-components/file-input';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [],
@@ -12,7 +16,11 @@ import { MatButtonModule } from '@angular/material/button';
   exports: [
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule
+    //NgxMatFileInputModule
   ]
 })
 export class MaterialModule { }
