08e208d6f93741584c1a452ca3dda0085099bb27
davidmr163 <davidmonteroruiz163@gmail.com>
2022-08-25 17:45:06 +0200

    add(code): some fields added

--- back/src/modules/users/interfaces/user.interface.ts
+++ back/src/modules/users/interfaces/user.interface.ts
@@ -3,6 +3,8 @@ import { Shelter } from "src/modules/shelters/interfaces/shelter.interface"
 
 export interface User{
     id: number
+    name: string
+    surname: string
     username: string
     email: string
     rol: string
