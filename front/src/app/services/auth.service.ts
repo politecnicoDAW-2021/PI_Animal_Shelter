ac3a039314a00a442b2ae29afeff0060e08d6ed5
davidmr163 <davidmonteroruiz163@gmail.com>
2022-08-29 20:11:00 +0200

    feat(app): register first approach

---
+++ front/src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  endpoint: string = 'http://localhost:3000'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  register(user: any){
+    return this.http.post(`${this.endpoint}/register`, user)
+  }
+}
