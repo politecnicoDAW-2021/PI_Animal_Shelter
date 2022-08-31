b2d53e71dbdd0da74f1a7931c44858ac85238bf2
davidmr163 <davidmonteroruiz163@gmail.com>
2022-08-31 19:24:36 +0200

    build(back): adding jwt config

--- back/src/auth/jwt/auth.module.ts
+++ back/src/auth/jwt/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { UserEntity } from 'src/database/entities/user/user.entity';
 import { UsersService } from 'src/modules/users/services/users.service';
 import { UserModule } from 'src/modules/users/user.module';
@@ -6,9 +7,15 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-    imports: [UserModule],
-    providers: [AuthService],
+    imports: [
+        UserModule,
+        JwtModule.register({
+            secret: 'secret',
+            signOptions: {expiresIn: '1d'}
+        })
+    ],
+    providers: [AuthService, JwtService],
     controllers: [AuthController],
-    exports: [AuthService]
+    exports: [AuthService, JwtService]
 })
 export class AuthModule {}
