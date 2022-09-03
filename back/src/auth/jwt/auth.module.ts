eb8f28330864c41ce2a13e63db4ce71cd1402443
davidmr163 <davidmonteroruiz163@gmail.com>
2022-09-03 16:11:08 +0200

    build(back): new modules for jwt

--- back/src/auth/jwt/auth.module.ts
+++ back/src/auth/jwt/auth.module.ts
@@ -5,17 +5,36 @@ import { UsersService } from 'src/modules/users/services/users.service';
 import { UserModule } from 'src/modules/users/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+
 
 @Module({
     imports: [
         UserModule,
         JwtModule.register({
             secret: 'secret',
-            signOptions: {expiresIn: '1d'}
+            signOptions: 
+            { 
+                expiresIn: '1d'
+            }
+        }),
+
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+
+        PassportModule.register({
+            defaultStrategy: 'jwt',
+            property: 'user',
+            session: false
         })
+        
     ],
-    providers: [AuthService, JwtService],
+    providers: [AuthService, JwtService, LocalStrategy, JwtStrategy],
     controllers: [AuthController],
-    exports: [AuthService, JwtService]
+    exports: [AuthService]
 })
 export class AuthModule {}
