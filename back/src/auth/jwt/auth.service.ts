85cdd77e0a5b7cf1299e29381de0c2981fa0f5b3
davidmr163 <davidmonteroruiz163@gmail.com>
2022-09-03 16:12:36 +0200

    feat(back): returning jwt token when logged in

--- back/src/auth/jwt/auth.service.ts
+++ back/src/auth/jwt/auth.service.ts
@@ -3,12 +3,15 @@ import { User } from "src/modules/users/interfaces/user.interface";
 import { PasswordService } from "src/modules/users/services/password.service";
 import { UsersService } from "src/modules/users/services/users.service";
 import * as bcrypt from 'bcrypt';
+import { JwtService } from "@nestjs/jwt";
+
 
 @Injectable()
 export class AuthService {
     constructor(
         private userService: UsersService,
-        private passwordService: PasswordService
+        private passwordService: PasswordService,
+        private jwtService: JwtService
     ) {}
 
     async register(user: any){       
@@ -41,7 +44,37 @@ export class AuthService {
     }   
 
     async login(user: any){
-        
+
+        const userId = await this.userService.findOneByEmail(user.email)
+        const payload = { email: user.email, sub: userId.id }
+
+        return {
+            username: user.email,
+            id: userId.id,
+            access_token: this.jwtService.sign(payload, { secret: 'secret' })
+        }
+    }
+
+    async validateUser(email: any, password: any): Promise<any> {
+        const user = this.userService.findOneByEmail(email)
+        const pass = this.passwordService.findOne(password)
+
+        if(user && await this.passwordsAreEqual((await pass).password, password)) {
+            return {
+                username: (await user).name,
+                password: password
+            }
+        }
+
+        return null
+    }
+
+
+    private async passwordsAreEqual(
+        hashedPassword: string,
+        plainPassword: string
+    ): Promise<boolean> {
+        return bcrypt.compare(plainPassword, hashedPassword)
     }
 
 } 
