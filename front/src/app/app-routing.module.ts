0e7c1f6d00f20791e0f2da7b6b7a7626792aa4da
davidmr163 <davidmonteroruiz163@gmail.com>
2022-09-04 22:14:02 +0200

    build(front): new components

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -6,6 +6,30 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./components/register/register.module').then(m => 
       m.RegisterModule)
+  },
+
+  {
+    path: '',
+    redirectTo: 'register',
+    pathMatch: 'full'
+  },
+  
+  {
+    path: 'home',
+    loadChildren: () => import('./components/landing-page/landing-page.module').then(m =>
+      m.LandingPageModule)
+  },
+  
+  {
+    path: 'me',
+    loadChildren: () => import('./components/about-me/about-me.module').then(m => 
+      m.AboutMeModule)
+  },
+
+  {
+    path: 'login',
+    loadChildren: () => import('./components/login/login.module').then(m => 
+      m.LoginModule)
   }
 ];
 
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,16 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterModule } from './components/register/register.module';
+import { AboutMeModule } from './components/about-me/about-me.module';
+import { LoginModule } from './components/login/login.module';
 
 @NgModule({
-  declarations: [
+  declarations:
+  [
     AppComponent
   ],
-  imports: [
+  imports: 
+  [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule,    
+    NoopAnimationsModule,
+    HttpClientModule,
+    RegisterModule,
+    AboutMeModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
