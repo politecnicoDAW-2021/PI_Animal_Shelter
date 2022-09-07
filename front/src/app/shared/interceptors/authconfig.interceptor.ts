fa23fc2ada59a529b1562b25bc51fd67ce37b8a3
davidmr163 <davidmonteroruiz163@gmail.com>
2022-09-07 20:17:50 +0200

    routes(front): interceptor WIP

---
+++ front/src/app/shared/interceptors/authconfig.interceptor.ts
@@ -0,0 +1,43 @@
+
+import {Injectable} from "@angular/core";
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpStatusCode} from "@angular/common/http";
+import { AuthService } from "src/app/services/auth.service";
+import { catchError, Observable, throwError } from "rxjs";
+import { Router } from "@angular/router";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor{
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) {
+  }
+
+  intercept(
+    req: HttpRequest<any>, 
+    next: HttpHandler, ): any {
+        const authToken = sessionStorage.getItem('token')
+        console.log('authToken', authToken);
+
+        if (authToken) {
+        req = req.clone({
+            setHeaders: {
+                Authorization: "Bearer " + authToken
+            },
+        });
+        }
+
+        return next.handle(req).pipe(
+        
+        catchError((err: HttpErrorResponse) => {
+            if(err.status === HttpStatusCode.Unauthorized){
+            this.authService.logout();
+            this.router.navigate(['login']);
+            }
+            return throwError(err);
+        })
+        )
+    } 
+
+    
+}
