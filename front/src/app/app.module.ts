d72e606dae508f79130013fbddf80e07a8468b9e
davidmr163 <davidmonteroruiz163@gmail.com>
2022-09-07 19:46:52 +0200

    update(app): app module

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,19 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
 import { AnimalIndividualComponent } from './components/animal-individual/animal-individual.component';
 
 
-
-
-=======
 import { HttpClientModule } from '@angular/common/http';
 import { RegisterModule } from './components/register/register.module';
 import { AboutMeModule } from './components/about-me/about-me.module';
 import { LoginModule } from './components/login/login.module';
 import { LandingPageModule } from './components/landing-page/landing-page.module';
->>>>>>> c19af5db86b5e51da3a291a64061ae69f724bde3
 
 @NgModule({
   declarations: [
@@ -26,16 +21,13 @@ import { LandingPageModule } from './components/landing-page/landing-page.module
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     NoopAnimationsModule,    
-=======
     NoopAnimationsModule,
     HttpClientModule,
     RegisterModule,
     AboutMeModule,
     LoginModule,
     LandingPageModule
->>>>>>> c19af5db86b5e51da3a291a64061ae69f724bde3
   ],
   providers: [],
   bootstrap: [AppComponent]
