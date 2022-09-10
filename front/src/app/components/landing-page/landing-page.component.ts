60306e9e0cd0cdc931247445574b77c97d2add10
laranatoli <franciscolaranatoli@gmail.com>
2022-09-10 12:19:49 +0200

    feat(front): carousel scheme

--- front/src/app/components/landing-page/landing-page.component.ts
+++ front/src/app/components/landing-page/landing-page.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {
+  MatCarouselSlideComponent,
+  Orientation
+} from '@ngbmodule/material-carousel';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent {
+   public slidesList = new Array<never>(5);
+  public showContent = false;
 
-  constructor() { }
+  public parentHeight = 'auto';
+  public timings = '250ms ease-in';
+  public autoplay = true;
+  public interval = 5000;
+  public loop = true;
+  public hideArrows = false;
+  public hideIndicators = false;
+  public color: ThemePalette = 'accent';
+  public maxWidth = 'auto';
+  public maintainAspectRatio = true;
+  public proportion = 25;
+  public slideHeight = '200px';
+  public slides = this.slidesList.length;
+  public overlayColor = '#00000040';
+  public hideOverlay = false;
+  public useKeyboard = true;
+  public useMouseWheel = true;
+  public orientation: Orientation = 'ltr';
+  public log: string[] = [];
 
-  ngOnInit(): void {
+  @ViewChildren(MatCarouselSlideComponent)
+  public carouselSlides!: QueryList<
+    MatCarouselSlideComponent
+  >;
+ 
+  public darkMode = false;
+
+  constructor(
+    private snackBar: MatSnackBar,
+    private overlayContainer: OverlayContainer,
+    private elementRef: ElementRef<HTMLElement>
+  ) {}
+
+
+
+  
+
+  public onChange(index: number) {
+    this.log.push(`MatCarousel#change emitted with index ${index}`);
   }
-
 }
--- front/src/app/components/landing-page/landing-page.module.ts
+++ front/src/app/components/landing-page/landing-page.module.ts
@@ -4,16 +4,34 @@ import { CommonModule } from '@angular/common';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
 import { MaterialModule } from 'src/app/shared/material/material.module';
-
-
+import { MatCarouselModule } from '@ngbmodule/material-carousel';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 @NgModule({
-  declarations: [
-    LandingPageComponent
-  ],
+  declarations: [LandingPageComponent],
   imports: [
     CommonModule,
     LandingPageRoutingModule,
-    MaterialModule
-  ]
+    MaterialModule,
+    MatCarouselModule.forRoot(),
+    MatExpansionModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatTooltipModule, MatCardModule,
+    MatCheckboxModule,
+  ],
 })
-export class LandingPageModule { }
+export class LandingPageModule {}
