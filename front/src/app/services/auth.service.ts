44ea0086be3e1866d9898b2a113a3b389b2a46e0
davidmr163 <davidmonteroruiz163@gmail.com>
2022-09-10 20:33:16 +0200

    build(front): auth service wip

--- front/src/app/services/auth.service.ts
+++ front/src/app/services/auth.service.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { map, pipe } from 'rxjs';
+import { SESSION_STORAGE, StorageService } from 'ngx-webstorage-service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +13,55 @@ export class AuthService {
   endpoint: string = 'http://localhost:3000'
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router,
+    @Inject(SESSION_STORAGE) private storage: StorageService
   ) { }
 
   register(user: any){
     return this.http.post(`${this.endpoint}/register`, user)
   }
 
-  login(user: any){
-    return this.http.post(`${this.endpoint}/login`, user)
+  login(user: any) {
+    return this.http
+      .post<any>(`${this.endpoint}/login`, user)
+      .pipe(
+        map(async (user) => {
+          if (user && user.access_token) { 
+            await (this.saveToken(user.access_token))
+          }
+        })
+      );
+  }
+
+  saveToken(access_token: string){
+    localStorage.setItem('token', access_token)
+  }
+
+  saveUser(user: string){
+    localStorage.setItem('user', user)
+  }
+
+  saveId(id: any){
+    localStorage.setItem('id', id)
+  }
+
+  getToken(){
+    return localStorage.getItem('token')
+  }
+
+  isLoggedIn(){
+    const authToken = localStorage.getItem('token')
+
+    return authToken !== null
+  }
+
+  logout() {
+    const removedToken = localStorage.removeItem('token');
+    const removedUser = localStorage.removeItem('user');
+    const removeIdUser = localStorage.removeItem('id');
+    if (removedToken == null) {
+      this.router.navigate(['login']);
+    }
   }
 }
