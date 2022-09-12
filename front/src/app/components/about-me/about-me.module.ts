4acbbcb837842cd40c0fde73270dcf05b68cfb6c
davidmr163 <davidmonteroruiz163@gmail.com>
2022-09-12 19:42:52 +0200

    feat(app): about-me comp first appr

--- front/src/app/components/about-me/about-me.module.ts
+++ front/src/app/components/about-me/about-me.module.ts
@@ -3,6 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AboutMeRoutingModule } from './about-me-routing.module';
 import { AboutMeComponent } from './about-me.component';
+import { MaterialModule } from 'src/app/shared/material/material.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
@@ -11,7 +18,14 @@ import { AboutMeComponent } from './about-me.component';
   ],
   imports: [
     CommonModule,
-    AboutMeRoutingModule
+    AboutMeRoutingModule,
+    MaterialModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatListModule
   ]
 })
 export class AboutMeModule { }
