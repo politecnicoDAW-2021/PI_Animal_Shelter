15394e59dd95c27b4838b960d91aae378d3ed833
davidmr163 <davidmonteroruiz163@gmail.com>
2022-09-12 19:46:51 +0200

    build(back): new controller import

--- back/src/modules/users/user.module.ts
+++ back/src/modules/users/user.module.ts
@@ -2,11 +2,13 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { PasswordEntity } from "src/database/entities/user/password.entity";
 import { UserEntity } from "src/database/entities/user/user.entity";
+import { UserController } from "./controller/user.controller";
 import { PasswordService } from "./services/password.service";
 import { UsersService } from "./services/users.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([UserEntity, PasswordEntity])],
+    controllers: [UserController],
     providers: [UsersService, PasswordService],
     exports: [UsersService, PasswordService]
 })
