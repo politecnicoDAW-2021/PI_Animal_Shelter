7085e5088f020d5f4fc6cb92c2a5f80f51f73957
davidmr163 <davidmonteroruiz163@gmail.com>
2022-09-12 19:42:09 +0200

    fix(app): providers removed

--- front/src/app/components/login/login.module.ts
+++ front/src/app/components/login/login.module.ts
@@ -3,11 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-import { Routes } from '@angular/router';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from 'src/app/shared/interceptors/authconfig.interceptor';
 
 @NgModule({
   declarations: [
@@ -16,15 +13,10 @@ import { AuthInterceptor } from 'src/app/shared/interceptors/authconfig.intercep
   imports: [
     CommonModule,
     LoginRoutingModule,
-    HttpClientModule,
     MaterialModule,
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [{
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptor,
-    multi: true
-  }],
+  providers: [],
 })
 export class LoginModule { }
