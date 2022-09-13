93b91b022017c22f9aa79355a4ca0332462d43be
davidmr163 <davidmonteroruiz163@gmail.com>
2022-09-13 13:39:41 +0200

    feat(back): middleware added

--- back/src/auth/jwt/auth.module.ts
+++ back/src/auth/jwt/auth.module.ts
@@ -1,14 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { JwtModule, JwtService } from '@nestjs/jwt';
-import { UserEntity } from 'src/database/entities/user/user.entity';
-import { UsersService } from 'src/modules/users/services/users.service';
 import { UserModule } from 'src/modules/users/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
+import { AuthMiddleware } from './middleware/auth.middleware';
 
 
 @Module({
@@ -37,4 +36,11 @@ import { PassportModule } from '@nestjs/passport';
     controllers: [AuthController],
     exports: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule implements NestModule{
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+          .apply(AuthMiddleware)
+          .forRoutes({ path: '*', method: RequestMethod.ALL });
+    }
+    
+}
