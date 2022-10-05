70ddcd25a0bbb2e3fc263da29520a80a12699af9
laranatoli <franciscolaranatoli@gmail.com>
2022-10-05 20:11:18 +0200

    refactor(back): app module updated

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,18 +3,19 @@ import { AppService } from './app.service';
 import { typeOrmConfig } from './config/typeOrm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { AnimalEntity } from './database/entities/animal/animal.entity';
-import { SpecieEntity } from './database/entities/animal/specie.entity';
-import { ShelterEntity } from './database/entities/shelter/shelter.entity';
-import { CreditCardEntity } from './database/entities/user/credit_card.entity';
-import { PasswordEntity } from './database/entities/user/password.entity';
-import { WishlistEntity } from './database/entities/common/wishlist.entity';
-import { UserEntity } from './database/entities/user/user.entity';
-import { SocialMediaEntity } from './database/entities/shelter/social_media.entity';
-import { MediaEntity } from './database/entities/animal/media.entity';
 
 import { AuthModule } from './auth/jwt/auth.module';
 import { UserModule } from './modules/users/user.module';
+import { AnimalEntity } from './database/entities/animal/animal.entity';
+import { MediaEntity } from './database/entities/animal/media.entity';
+import { SpecieEntity } from './database/entities/animal/specie.entity';
+import { WishlistEntity } from './database/entities/common/wishlist.entity';
+import { ShelterEntity } from './database/entities/shelter/shelter.entity';
+import { SocialMediaEntity } from './database/entities/shelter/social_media.entity';
+import { CreditCardEntity } from './database/entities/user/credit_card.entity';
+import { PasswordEntity } from './database/entities/user/password.entity';
+import { UserEntity } from './database/entities/user/user.entity';
+import { AnimalModule } from './modules/animals/modules/animal.module';
 
 @Module({
   imports: [
@@ -26,12 +27,13 @@ import { UserModule } from './modules/users/user.module';
       UserEntity,
       SocialMediaEntity,
       ShelterEntity,
-      AnimalEntity,
-      SpecieEntity,
       MediaEntity,
+      SpecieEntity,
+      AnimalEntity,
     ]),
     UserModule,
     AuthModule, 
+    AnimalModule
   ],
   controllers: [AppController],
   providers: [AppService],
