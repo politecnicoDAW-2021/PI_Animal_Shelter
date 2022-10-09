a8afdb3d736ad1c4abda3c834589c8be335dcb18
davidmr163 <davidmonteroruiz163@gmail.com>
2022-10-09 19:30:25 +0200

    hotfix(back): findOne method working

--- back/src/modules/users/services/password.service.ts
+++ back/src/modules/users/services/password.service.ts
@@ -1,24 +1,24 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { PasswordEntity } from "src/database/entities/user/password.entity";
-import { Repository } from "typeorm";
+import { Injectable } from '@nestjs/common';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { PasswordEntity } from 'src/database/entities/user/password.entity';
+import { Repository } from 'typeorm';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class PasswordService {
-    constructor(
-        @InjectRepository(PasswordEntity)
-        private readonly passwordRepository: Repository<PasswordEntity>
-    ) {}
-    
-    async create(password: any): Promise<PasswordEntity>{        
-        return await this.passwordRepository.save(password)
-    }
+  constructor(
+    @InjectRepository(PasswordEntity)
+    private readonly passwordRepository: Repository<PasswordEntity>,
+    @InjectDataSource() private dataSource: DataSource,
+  ) {}
 
-    async findOne(condition: any): Promise<PasswordEntity | any> {
-        return await this.passwordRepository.findOneBy({
-            password: condition
-        })
-    }
+  async create(password: any): Promise<PasswordEntity> {
+    return await this.passwordRepository.save(password);
+  }
 
-
+  async findOne(condition: any): Promise<PasswordEntity | any> {
+    return await this.dataSource.query(
+      `select password from password where userId = '${condition.id}'`,
+    );
+  }
 }
