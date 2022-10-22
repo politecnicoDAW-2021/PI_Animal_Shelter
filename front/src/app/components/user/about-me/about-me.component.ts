928cf5b6b2594ce98d93fe631737ba94ce57995b
davidmr163 <davidmonteroruiz163@gmail.com>
2022-10-22 18:40:08 +0200

    feat(front): new user info layout

--- front/src/app/components/user/about-me/about-me.component.ts
+++ front/src/app/components/user/about-me/about-me.component.ts
@@ -5,26 +5,22 @@ import { UserService } from 'src/app/services/users/user.service';
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.css']
+  styleUrls: ['./about-me.component.css'],
 })
 export class AboutMeComponent implements OnInit {
+  constructor(private userService: UserService) {}
 
-  constructor(
-    private userService: UserService
-  ) { }
-
-  public user?: any
+  public user?: any;
 
   ngOnInit(): void {
-   this.user = this.getUserInfo()
-   
+    this.user = this.getUserInfo();
   }
 
-  getUserInfo(){
-    const id = localStorage.getItem('id')
+  getUserInfo() {
+    const id = localStorage.getItem('id');
 
-    return this.userService.findById(id)
-        .subscribe(data => this.user = data)
+    return this.userService
+      .findById(id)
+      .subscribe((data) => (this.user = data));
   }
-
 }
