5160de683d90367dac4c8c4d97b2c6a886cc33ab
davidmr163 <davidmonteroruiz163@gmail.com>
2022-10-31 18:15:27 +0100

    fix(back): middleware accepting google tokens

--- back/src/auth/jwt/middleware/auth.middleware.ts
+++ back/src/auth/jwt/middleware/auth.middleware.ts
@@ -1,44 +1,37 @@
-import { 
-    ForbiddenException, 
-    Injectable, 
-    NestMiddleware 
-} from "@nestjs/common";
-import { verify } from 'jsonwebtoken';
-import { UsersService } from "src/modules/users/services/users.service";
+import { ForbiddenException, Injectable, NestMiddleware } from '@nestjs/common';
+import { access } from 'fs';
+import { verify, decode, sign } from 'jsonwebtoken';
+import { UsersService } from 'src/modules/users/services/users.service';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-    constructor(
-        private readonly userService: UsersService
-    ) {}
+  constructor(private readonly userService: UsersService) {}
 
+  async use(req: any, res: any, next: (error?: any) => void) {
+    const bearerHeader = req.headers.authorization;
+    const accessToken = bearerHeader && bearerHeader.split(' ')[1];
+    let userEmail;
 
-    async use(req: any, res: any, next: (error?: any) => void) {
-        const bearerHeader = req.headers.authorization;
-        const accessToken = bearerHeader && bearerHeader.split(' ')[1]
-        let userEmail;
+    if (!bearerHeader || !accessToken) {
+      console.log('no hay tokens');
 
-        if(!bearerHeader || !accessToken) {
-            console.log('no hay tokens');
-            
-            return next();
-        }
-
-        try{
-            const { email }: any = verify(
-                accessToken,
-                'secret'
-            )
-
-            userEmail = await this.userService.findOneByEmail(email)
-        } catch (error) {
-            throw new ForbiddenException('Please register or sign in')
-        }
-
-        if(userEmail){
-            req.user = userEmail
-        }
-
-        next();
+      return next();
     }
+
+    const signedToken = sign(accessToken, 'secret');
+
+    try {
+      const { email }: any = verify(signedToken, 'secret');
+
+      userEmail = await this.userService.findOneByEmail(email);
+    } catch (error) {
+      throw new ForbiddenException('Please register or sign in');
+    }
+
+    if (userEmail) {
+      req.user = userEmail;
+    }
+
+    next();
+  }
 }
