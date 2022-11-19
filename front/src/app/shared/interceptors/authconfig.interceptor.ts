59962a788a68d7fdbc3472ba79a8b0cb29f58ed1
davidmr163 <davidmonteroruiz163@gmail.com>
2022-11-19 02:44:19 +0100

    fix(front): remove token clg

--- front/src/app/shared/interceptors/authconfig.interceptor.ts
+++ front/src/app/shared/interceptors/authconfig.interceptor.ts
@@ -1,45 +1,41 @@
-
-import {Inject, Injectable} from "@angular/core";
-import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpStatusCode} from "@angular/common/http";
-import { AuthService } from "src/app/services/auth.service";
-import { catchError, Observable, throwError } from "rxjs";
-import { Router } from "@angular/router";
+import { Inject, Injectable } from '@angular/core';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpStatusCode,
+} from '@angular/common/http';
+import { AuthService } from 'src/app/services/auth.service';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthInterceptor implements HttpInterceptor{
-    
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-    ) {
-  }
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  intercept(
-    req: HttpRequest<any>, 
-    next: HttpHandler, ): any {        
-        const authToken = this.authService.getToken()
-        console.log('authToken', authToken);   
+  intercept(req: HttpRequest<any>, next: HttpHandler): any {
+    const authToken = this.authService.getToken();
 
-        if (authToken) {
-        req = req.clone({
-            setHeaders: {
-                Authorization: "Bearer " + authToken
-            },
-        });
-        }
-
-        return next.handle(req).pipe(
-        
-        catchError((err: HttpErrorResponse) => {
-            if(err.status === HttpStatusCode.Unauthorized){
-            this.authService.logout();
-            this.router.navigate(['login']);
-            }
-            return throwError(err);
-        })
-        )
+    if (authToken) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + authToken,
+        },
+      });
     }
-    
+
+    return next.handle(req).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === HttpStatusCode.Unauthorized) {
+          this.authService.logout();
+          this.router.navigate(['login']);
+        }
+        return throwError(err);
+      })
+    );
+  }
 }
