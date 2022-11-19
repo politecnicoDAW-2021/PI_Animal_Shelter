a8de4c919199eecbfd67cf9d4da693c7b9428749
laranatoli <franciscolaranatoli@gmail.com>
2022-11-19 02:33:52 +0100

    feat(front): shelter services

---
+++ front/src/app/services/shelter/shelter.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShelterService {
+  private endpoint: string = 'http://localhost:3000/shelter';
+
+  constructor(private http: HttpClient) {}
+
+  getShelter = (): Observable<any[]> => {
+    return this.http.get<any[]>(`${this.endpoint}`);
+  };
+}
