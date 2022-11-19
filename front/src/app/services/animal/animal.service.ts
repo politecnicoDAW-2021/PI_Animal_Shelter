bf4b72e102b47c62d001b348a6e0dd2f2c5d78f0
laranatoli <franciscolaranatoli@gmail.com>
2022-11-19 02:34:18 +0100

    feat(front): species endpoint

--- front/src/app/services/animal/animal.service.ts
+++ front/src/app/services/animal/animal.service.ts
@@ -8,26 +8,26 @@ import { Observable } from 'rxjs';
 export class AnimalService {
   private endpoint: string = 'http://localhost:3000/animal';
   private endpointBreeds: string = 'http://localhost:3000/breeds';
+  private endpointSpecies: string = 'http://localhost:3000/species';
 
   constructor(private http: HttpClient) {}
 
   getBreeds = (): Observable<any[]> => {
     return this.http.get<any[]>(`${this.endpointBreeds}`);
   };
-
+  getSpecies = (): Observable<any[]> => {
+    return this.http.get<any[]>(`${this.endpointSpecies}`);
+  };
   getAnimalByParams = (params: any) => {
     let query = '';
     const asArray = Object.entries(params);
-    params = asArray.filter(([key, value]) => {
-      return value !== undefined;
-    });
+    params = asArray.filter(([_, value]) => value);
     if (params.length) {
       console.log(params);
       query = Object.keys(params)
-        .reduce(function (accumulator, key) {
-          return accumulator != '?'
-            ? accumulator.toString() + '&' + params[key].toString()
-            : accumulator.toString() + params[key].toString();
+        .reduce((accumulator, key) => {
+          const option = accumulator !== '?' ? '&' : '';
+          return `${accumulator}${option}${params[key]}`;
         }, '?')
         .replace(/,/g, '=');
     }
