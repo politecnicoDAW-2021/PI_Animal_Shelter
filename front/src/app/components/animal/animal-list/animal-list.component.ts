4effefddd505cb423461e1ebd51e4c708614c6ad
laranatoli <franciscolaranatoli@gmail.com>
2022-11-19 15:08:35 +0100

    feat(front):  animal list working with filter form

--- front/src/app/components/animal/animal-list/animal-list.component.ts
+++ front/src/app/components/animal/animal-list/animal-list.component.ts
@@ -1,67 +1,122 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnimalService } from '@services/animal/animal.service';
-import { FormControl } from '@angular/forms';
-import { map, startWith } from 'rxjs';
+import {
+  FormBuilder,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
 import { AnimalCardComponent } from '../animal-card/animal-card.component';
+import { ShelterService } from '@services/shelter/shelter.service';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatOptionModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-animal-list',
   standalone: true,
-  imports: [CommonModule, AnimalCardComponent],
+  imports: [
+    CommonModule,
+    AnimalCardComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    MatOptionModule,
+    MatAutocompleteModule,
+  ],
   templateUrl: './animal-list.component.html',
   styleUrls: ['./animal-list.component.css'],
 })
 export class AnimalListComponent implements OnInit {
-  // @Input() dog!: any;
-  // @Input() index!: any;
-  animals: any[] = [];
-  // options: any[] = [];
-  // filteredOptions: any;
-  // myControl = new FormControl('');
+  animals: Observable<any[]> = this.getAnimals();
+  i: any = 0;
 
-  constructor(public animalService: AnimalService) {}
+  breeds: any[] = [];
+  cities: any[] = [];
+  species: any[] = [];
 
+  filteredBreeds: any;
+  filteredCities: any;
+  filteredSpecies: any;
+
+  myControl = new FormControl('');
+
+  constructor(
+    private fb: FormBuilder,
+    private animalService: AnimalService,
+    private shelterService: ShelterService
+  ) {}
+  filterForm = this.fb.group({
+    breed: [''],
+    specie: [''],
+    city: [''],
+  });
   ngOnInit(): void {
-    // console.log(this.getAnimals());
-    // this.getAnimals();
-    // console.log(this.animals);
-    // this.getBreeds();
-    // this.filteredOptions = this.myControl.valueChanges.pipe(
-    //   startWith(''),
-    //   map((value) => this._filter(value || ''))
-    // );
-    this.getAnimals();
+    //* Get Breeds */
+    this.getBreeds();
+    this.filteredBreeds = this.filterForm
+      .get('breed')
+      ?.valueChanges.pipe(
+        map((value: any) => this._filter(value ?? '', this.breeds))
+      );
+
+    //* Get Cities */
+    this.getCities();
+    this.filteredCities = this.filterForm
+      .get('city')
+      ?.valueChanges.pipe(
+        map((value: any) => this._filter(value ?? '', this.cities))
+      );
+
+    //* Get Species */
+    this.getSpecies();
+    this.filteredSpecies = this.filterForm
+      .get('specie')
+      ?.valueChanges.pipe(
+        map((value: any) => this._filter(value ?? '', this.species))
+      );
+    console.log('species: ', this.filteredSpecies);
+    console.log('cities: ', this.filteredCities);
   }
 
   getAnimals() {
-    return this.animalService.getAllAnimals().subscribe((animals) => {
-      this.animals = animals;
-      console.log('this.animals', this.animals);
-    });
+    return this.animalService.getAnimalByParams({});
+  }
+  getBreeds() {
+    this.animalService.getBreeds().subscribe((breed) => (this.breeds = breed));
   }
 
-  // getBreeds() {
-  //   this.animalService
-  //     .getBreeds()
-  //     .subscribe((animals) => (this.options = animals));
-  // }
+  getSpecies() {
+    this.animalService
+      .getSpecies()
+      .subscribe((specie) => (this.species = specie));
+  }
+  getCities() {
+    this.shelterService.getShelter().subscribe((city) => (this.cities = city));
+  }
 
-  // getAge = (date: string) => {
-  //   let today = new Date();
-  //   let birthDate = new Date(date);
-  //   let age = today.getFullYear() - birthDate.getFullYear();
-  //   let m = today.getMonth() - birthDate.getMonth();
-  //   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-  //     age--;
-  //   }
-  //   return age;
-  // };
-
-  // private _filter(value: string): string[] {
-  //   const filterValue = value.toLowerCase();
-  //   return this.options.filter((option) =>
-  //     option.name.toLowerCase().includes(filterValue)
-  //   );
-  // }
+  private _filter(value: string, elements: any[]): string[] {
+    console.log(elements);
+    const filterValue = value.toLowerCase();
+    return elements.filter((elements) =>
+      elements.name.toLowerCase().includes(filterValue)
+    );
+  }
+  getAge = (date: string) => {
+    let today = new Date();
+    let birthDate = new Date(date);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  };
+  public sendForm() {
+    this.animals = this.animalService.getAnimalByParams({
+      breed: this.filterForm.value.breed,
+      specie: this.filterForm.value.specie,
+      city: this.filterForm.value.city,
+    });
+  }
 }
