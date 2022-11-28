ec6b54786ac0a55de0785771ec22b653cdec1989
laranatoli <franciscolaranatoli@gmail.com>
2022-11-28 20:37:08 +0100

    feat(back): media entity

--- back/src/database/entities/animal/media.entity.ts
+++ back/src/database/entities/animal/media.entity.ts
@@ -6,7 +6,6 @@ import {
   ManyToOne,
 } from 'typeorm';
 import { AnimalEntity } from '../animal/animal.entity';
-import { UserEntity } from '../user/user.entity';
 
 @Entity('media')
 export class MediaEntity {
@@ -16,10 +15,10 @@ export class MediaEntity {
   @Column()
   filename: string;
 
-  // @Column({
-  //   type: 'bytea',
-  // })
-  // data: Uint8Array;
+  @Column({
+    type: 'longblob',
+  })
+  data: Uint8Array;
 
   @ManyToOne(() => AnimalEntity, (animal) => animal.id)
   animal: AnimalEntity;
