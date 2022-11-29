31b1fb2b80218041792b5600f83d84e717694568
davidmr163 <davidmonteroruiz163@gmail.com>
2022-11-29 23:33:19 +0100

    perf(back): minor changes

--- back/src/modules/animals/services/animal.service.ts
+++ back/src/modules/animals/services/animal.service.ts
@@ -26,18 +26,22 @@ export class AnimalService {
     @InjectRepository(MediaEntity)
     private readonly mediaRepository: Repository<MediaEntity>,
   ) {}
+
   async findBreeds(): Promise<BreedEntity[]> {
     return await this.breedRepository.find();
   }
+
   async findSpecies(): Promise<SpecieEntity[]> {
     return await this.specieRepository.find();
   }
+
   findCities(): Promise<AnimalEntity[]> {
     return this.animalRepository.find();
   }
+
   async findAll(query: any) {
     const file: any = await this.getFileById(query.id);
-    console.log(file);
+
     return await this.animalRepository.findBy({
       id: query.id,
       gender: query.gender,
@@ -46,8 +50,8 @@ export class AnimalService {
       breed: { specie: { id: query.specie }, id: query.breed },
     });
   }
+
   addAnimals(animal: any) {
-    console.log(animal);
     return this.animalRepository.insert({
       name: animal.name,
       gender: animal.gender,
@@ -60,18 +64,17 @@ export class AnimalService {
       description: animal.description,
     });
   }
+
   async uploadFiles(idAnimal: number, file: Buffer, filename: string) {
     const newFile = await this.mediaRepository.create({
       animal: { id: idAnimal },
       filename,
       data: file,
     });
-    console.log(newFile);
     return await this.mediaRepository.save(newFile);
   }
 
   async getFileById(id: any) {
-    console.log(id);
     const file = await this.mediaRepository.findOneBy({ id: id });
     // const stream = Readable.from(file.data);
     return file;
