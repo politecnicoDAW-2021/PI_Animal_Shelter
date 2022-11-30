dad4ff24ae012860a54c412440ff3fcfedb8fd46
davidmr163 <davidmonteroruiz163@gmail.com>
2022-11-30 23:11:02 +0100

    fix(db): remove wrong table field

--- back/src/database/entities/user/user.entity.ts
+++ back/src/database/entities/user/user.entity.ts
@@ -5,15 +5,13 @@ import {
   OneToMany,
   ManyToMany,
   ManyToOne,
-} 
-from 'typeorm';
+} from 'typeorm';
 
 import { WishlistEntity } from '../common/wishlist.entity';
 import { ShelterEntity } from '../shelter/shelter.entity';
 import { CreditCardEntity } from './credit_card.entity';
 import { PasswordEntity } from './password.entity';
 export type UserRol = 'admin' | 'user' | 'shelter';
-
 
 @Entity('user')
 export class UserEntity {
@@ -44,22 +42,22 @@ export class UserEntity {
 
   @Column()
   picture: string;
-  
+
   /* @OneToMany(() => PasswordEntity, (password) => password.id)
   password: PasswordEntity; */
 
-  @OneToMany(() => WishlistEntity, (wishlist) => wishlist.id , {
-    onDelete: 'CASCADE'
+  @OneToMany(() => WishlistEntity, (wishlist) => wishlist.id, {
+    onDelete: 'CASCADE',
   })
   wishlist: WishlistEntity;
 
   @OneToMany(() => CreditCardEntity, (creditCard) => creditCard.id, {
-    onDelete: 'CASCADE'
+    onDelete: 'CASCADE',
   })
   creditCard: CreditCardEntity;
 
-  @ManyToOne(() => ShelterEntity, (shelter) => shelter.id, {
-    onDelete: 'CASCADE'
-  })
-  shelter: ShelterEntity;
+  // @ManyToOne(() => ShelterEntity, (shelter) => shelter.id, {
+  //   onDelete: 'CASCADE'
+  // })
+  // shelter: ShelterEntity;
 }
