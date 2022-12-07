65506c54a656b5510eed8e0a4d110371fd06a9b0
davidmr163 <davidmonteroruiz163@gmail.com>
2022-12-08 00:06:12 +0100

    feat(front): add admin service

---
+++ front/src/app/services/admin.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminService {
+  private endpoint: string = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  getAnimals() {
+    return this.http.get<any[]>(`${this.endpoint}/count-animals`);
+  }
+
+  getAnimalsData() {
+    return this.http.get<any[]>(`${this.endpoint}/animal`);
+  }
+
+  getBreeds() {
+    return this.http.get<any[]>(`${this.endpoint}/count-breeds`);
+  }
+
+  getUsers() {
+    return this.http.get<any[]>(`${this.endpoint}/count-users`);
+  }
+
+  getUsersData() {
+    return this.http.get<any[]>(`${this.endpoint}/users`);
+  }
+
+  getShelters() {
+    return this.http.get<any[]>(`${this.endpoint}/count-shelters`);
+  }
+}
