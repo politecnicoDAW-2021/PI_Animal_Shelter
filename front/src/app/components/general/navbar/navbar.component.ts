76aa9a25ff9a143a3a2503fcd2e3bcedfce6efc4
davidmr163 <davidmonteroruiz163@gmail.com>
2022-12-09 23:50:32 +0100

    feat(app): add dropdown on navbar

--- front/src/app/components/general/navbar/navbar.component.ts
+++ front/src/app/components/general/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '@services/auth.service';
+import { UserService } from '@services/users/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,11 +9,48 @@ import { Router } from '@angular/router';
   styles: [],
 })
 export class NavbarComponent implements OnInit {
-  constructor(private router: Router) {}
+  public user?: any;
+  public text?: string;
 
-  ngOnInit(): void {}
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.user = this.getUserInfo();
+    console.log(this.user);
+    this.loggedIn();
+    this.refresh();
+  }
 
   getUrl() {
     return this.router.url;
+  }
+
+  getUserInfo() {
+    const email = localStorage.getItem('email');
+
+    return this.userService.findByEmail(email).subscribe((data) => {
+      this.user = data;
+      console.log(this.user);
+    });
+  }
+
+  loggedIn() {
+    return this.authService.isLoggedIn();
+  }
+
+  signIn() {
+    this.router.navigate(['login']);
+  }
+
+  logOut() {
+    this.authService.logout();
+  }
+
+  refresh() {
+    return this.userService.findByEmail(localStorage.getItem('email'));
   }
 }
