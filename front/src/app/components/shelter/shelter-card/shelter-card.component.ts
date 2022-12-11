e773b1c2c78d9875dc713f696f169fadfa51577f
davidmr163 <davidmonteroruiz163@gmail.com>
2022-12-11 14:59:53 +0100

    feat(front): shelter-card scaffolding

---
+++ front/src/app/components/shelter/shelter-card/shelter-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shelter-card',
+  templateUrl: './shelter-card.component.html',
+  styles: [
+  ]
+})
+export class ShelterCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
