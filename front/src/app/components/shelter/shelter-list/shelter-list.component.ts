507d3e1babf32b3231eb06fea44585f0519fb97c
davidmr163 <davidmonteroruiz163@gmail.com>
2022-12-11 15:48:43 +0100

    feat(front): shelter list working

--- front/src/app/components/shelter/shelter-list/shelter-list.component.ts
+++ front/src/app/components/shelter/shelter-list/shelter-list.component.ts
@@ -1,14 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FooterComponent } from '@components/general/footer/footer.component';
 import { ShelterService } from '@services/shelter/shelter.service';
+import { Observable } from 'rxjs';
+import { ShelterCardComponent } from '../shelter-card/shelter-card.component';
 
 @Component({
   standalone: true,
   selector: 'app-shelter-list',
   templateUrl: './shelter-list.component.html',
   styles: [],
+  imports: [CommonModule, ShelterCardComponent, FooterComponent],
 })
 export class ShelterListComponent implements OnInit {
-  shelters: any[] = [];
+  shelters: Observable<any[]> = this.getShelters();
+
   constructor(private shelterService: ShelterService) {}
 
   ngOnInit(): void {
@@ -16,9 +22,6 @@ export class ShelterListComponent implements OnInit {
   }
 
   getShelters() {
-    return this.shelterService.getShelter().subscribe((data) => {
-      this.shelters = data;
-      console.log(this.shelters);
-    });
+    return this.shelterService.getShelter();
   }
 }
