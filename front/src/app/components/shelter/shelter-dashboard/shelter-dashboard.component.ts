94b36474da6d23478a62f214e16a61571d0f0fb3
laranatoli <franciscolaranatoli@gmail.com>
2022-12-11 04:23:09 +0100

    feat(front): tables buttons and adoptions working

--- front/src/app/components/shelter/shelter-dashboard/shelter-dashboard.component.ts
+++ front/src/app/components/shelter/shelter-dashboard/shelter-dashboard.component.ts
@@ -13,21 +13,36 @@ export class ShelterDashboardComponent implements OnInit {
   constructor(private adoption: AdoptionService) {}
 
   adoptions?: any = [];
+  accepteds?: any = [];
 
   ngOnInit(): void {
     this.adoptions = this.getAnimals();
+    this.accepteds = this.getAccepteds();
   }
 
   getAnimals() {
     return this.adoption.getAdoptation().subscribe((adoption) => {
       this.adoptions = adoption;
-      console.log(this.adoptions);
     });
   }
+  getAccepteds() {
+    return this.adoption.getAcepteds().subscribe((accepted) => {
+      this.accepteds = accepted;
+    });
+  }
+
   deleteAnimal(id: number) {
-    return this.adoption.deleteAdoptation(id);
+    return this.adoption.deleteAdoptation(id).subscribe(() => {
+      this.adoptions.filter((adoption: any) => adoption.id !== id);
+      this.accepteds.filter((adoption: any) => adoption.id !== id);
+      this.getAnimals();
+    });
   }
   accept(id: number) {
-    return this.adoption.putAdoptation(id);
+    return this.adoption.putAdoptation(id).subscribe(() => {
+      this.accepteds.filter((adoption: any) => adoption.id !== id);
+      this.adoptions.filter((adoption: any) => adoption.id !== id);
+      this.getAccepteds();
+    });
   }
 }
