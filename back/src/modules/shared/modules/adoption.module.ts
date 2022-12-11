0847dacabf60e50f96a93c2d548e76e55aed105c
laranatoli <franciscolaranatoli@gmail.com>
2022-12-11 04:21:57 +0100

    feat(back): min fix

--- back/src/modules/shared/modules/adoption.module.ts
+++ back/src/modules/shared/modules/adoption.module.ts
@@ -1,8 +1,9 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
-import AdoptionEntity from 'src/database/entities/common/adoption.entity';
+
 import { Module } from '@nestjs/common';
 import { AdoptionController } from '../controllers/adoption.controller';
 import { AdoptionService } from '../services/adoption.service';
+import { AdoptionEntity } from 'src/database/entities/common/adoption.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AdoptionEntity])],
--- back/src/modules/shelters/controllers/shelter.controller.ts
+++ back/src/modules/shelters/controllers/shelter.controller.ts
@@ -2,15 +2,15 @@ import { Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { get } from 'http';
 import { ShelterService } from '../services/shelter.service';
 
-@Controller()
+@Controller('shelter')
 export class ShelterController {
   constructor(private readonly shelterService: ShelterService) {}
-  @Get('shelter')
+  @Get()
   shelters() {
     return this.shelterService.findCities();
   }
 
-  @Get('shelter/:email')
+  @Get('/:email')
   async shelterInfo(@Param('email') email: any) {
     return await this.shelterService.findOneByEmail(email);
   }
