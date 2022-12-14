003847892c712b519f388deff393d9f53ef91edd
davidmr163 <davidmonteroruiz163@gmail.com>
2022-12-14 03:46:09 +0100

    feat(services): add file handling services

--- back/src/modules/animals/services/animal.service.ts
+++ back/src/modules/animals/services/animal.service.ts
@@ -4,15 +4,16 @@ import {
   NotFoundException,
   StreamableFile,
 } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { distinct } from 'rxjs';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { Blob } from 'buffer';
+import { distinct, from, lastValueFrom, map, Observable } from 'rxjs';
 import { AnimalEntity } from 'src/database/entities/animal/animal.entity';
 import { BreedEntity } from 'src/database/entities/animal/breed.entity';
 import { MediaEntity } from 'src/database/entities/animal/media.entity';
 import { SpecieEntity } from 'src/database/entities/animal/specie.entity';
-import { Readable } from 'stream';
+import { DataSource } from 'typeorm';
 
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 
 @Injectable()
 export class AnimalService {
@@ -25,6 +26,7 @@ export class AnimalService {
     private readonly breedRepository: Repository<BreedEntity>,
     @InjectRepository(MediaEntity)
     private readonly mediaRepository: Repository<MediaEntity>,
+    @InjectDataSource() private dataSource: DataSource,
   ) {}
 
   async findBreeds(): Promise<BreedEntity[]> {
@@ -83,5 +85,84 @@ export class AnimalService {
       .select('shelter')
       .where('animal.id = ' + idAnimal)
       .getRawMany();
+  }
+
+  async saveAnimalPhoto(path: any): Promise<any[]> {
+    const result = await this.mediaRepository
+      .createQueryBuilder()
+      .update(MediaEntity)
+      .set({
+        filename: path,
+      })
+      .where({ id: 1 })
+      .returning('*')
+      .execute();
+
+    return result.raw;
+  }
+
+  updateAnimalPhoto(id: any, imagePath: any) {
+    // const blob = new Uint8Array();
+    // const media = new MediaEntity();
+    // (media.id = 1), (media.filename = imagePath);
+
+    // return this.mediaRepository.insert({
+    //   animal: { id: 3 },
+    //   filename: imagePath,
+    // });
+    // return this.animalRepository.insert({
+    //   id: id,
+    //   filename: imagePath,
+    // });
+    return this.animalRepository.update(id, { filename: imagePath });
+  }
+
+  // async findImageByAnimalId(): Promise<Observable<any>> {
+  //   // return from(await this.dataSource.query(
+  //   //   `select filename from media where animalId = '${1}'`,
+  //   // ));
+  //   const id: any = 1;
+  //   return from(this.mediaRepository.findOneBy({ id: 1 })).pipe(
+  //     map((animal: any) => {
+  //       return animal.imagePath;
+  //     }),
+  //   );
+  // }
+
+  findImageByAnimalId(id: any): Observable<string> | any {
+    // return from(this.mediaRepository.findOneBy({ id: 32 })).pipe(
+    //   map((animal: any) => {
+    //     //delete user.password;
+    //     console.log('animal', animal);
+
+    //     return animal.filename;
+    //   }),
+    // );
+
+    // return from(this.animalRepository.findBy({ id: id })).pipe(
+    //   map((animal: any) => {
+    //     console.log('animal', animal);
+
+    //     return animal.filename;
+    //   }),
+    // );
+
+    // return from(this.animalRepository.findBy({ id: id })).pipe(
+    //   map((animal: any) => {
+    //     console.log('animal', animal);
+    //     return animal.filename;
+    //   }),
+    // );
+    console.log('id', id);
+
+    const animal = this.animalRepository.findOneBy({ id: id });
+    console.log('animal', animal);
+
+    return from(animal).pipe(
+      map((an: any) => {
+        console.log('an', an);
+        return an.filename;
+      }),
+    );
   }
 }
